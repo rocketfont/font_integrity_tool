90e4c87c2f045df84b99be22d0c5df62277d8b09
ajou201420994 <zola8138@ajou.ac.kr>
2020-11-02 21:08:24 +0900

    Code Improvement, check result changes

--- FontIntegrity.py
+++ FontIntegrity.py
@@ -35,7 +35,6 @@ if not os.path.exists(fontname):
         print("The font does not exist.")
         sys.exit(-2)
 
-
 if len(sys.argv) == 2:
     minGlyph = 0xAC00
     maxGlyph = 0xD7A3
@@ -51,13 +50,15 @@ if minGlyph > maxGlyph:
     print("The range is incorrect.")
     sys.exit(-2)
 
-if platform.system() == "Windows":
-    shcmd = "ttx -t cmap " + "\"" + fontname + "\""
-
-else:
-    shcmd = "ttx -t cmap " + "\"" + fontname + "\""
-
-os.popen(shcmd).read()
+if os.path.exists(xml_name):
+    print('TTX file for this font found!')
+else: ## if TTX exists then use it
+    print('TTX file for this font not found. Making new TTX file.')
+    if platform.system() == "Windows": ## Windows CMD
+        shcmd = "ttx -t cmap " + "\"" + fontname + "\""
+    else: ## POSIX CMD
+        shcmd = "ttx -t cmap " + "\"" + fontname + "\""
+    os.popen(shcmd).read()
 
 ## TTX-XML PART ##
 
@@ -84,22 +85,25 @@ print(codeAry[0:128])
 
 ## IMAGE PART ##
 
-fnt = ImageFont.truetype(fontname, 15)
+fnt = ImageFont.truetype(fontname, 24)
 allowedBlank = [0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x20, 0x85, 0xA0, 0x1680, 0x180E, 0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005, 0x2006, 0x2007, 0x2008, 0x2009, 0x200A, 0x200B, 0x200C, 0x200D, 0x2028, 0x2029, 0x202F, 0x205F, 0x2060, 0x3000, 0xFEFF]
 numofEmpty = 0
 numofExist = 0
 
 txt_file = open(exist_glyph_text, mode='wt', encoding='utf-8')
 
+img_width = 48
+img_height = 48
+
 for codenum in codeAry:
 
     if codenum < 0x20: ## ignore control chars and space
         continue
     
-    ## Make a 24x24 image
-    img = Image.new('RGB', (24, 24), color = 'white')
+    ## Make an image
+    img = Image.new('RGB', (img_width, img_height), color = 'white')
     drawer = ImageDraw.Draw(img)
-    drawer.text((4,4), chr(codenum), font=fnt, fill=(0,0,0))
+    drawer.text((8,8), chr(codenum), font=fnt, fill=(0,0,0))
 
     ## print(img)
     
@@ -108,8 +112,8 @@ for codenum in codeAry:
     
     pixels = []
 
-    for x in range(24):
-        for y in range(24):
+    for x in range(img_width):
+        for y in range(img_height):
             pixels.append(img.getpixel((x,y)))
 
     isBlank = True
@@ -130,6 +134,7 @@ for codenum in codeAry:
     else:
         txt_file.write("%d\n" % codenum)
         numofExist += 1
+
 
 
 ##for code in existCodeAry:
